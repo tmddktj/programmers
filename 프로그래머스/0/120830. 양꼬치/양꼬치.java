fad45ad370e31cc316fe6c4978430e6e351b591c
tmddktj <tmddktj@gmail.com>
2025-11-26 00:29:20 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        if (0 < n && n < 10 && (n/10) <= k && k < 1000){
+            answer = n * 12000 + k * 2000;
+        } else if (10 <= n && n < 1000 && (n/10) <= k && k < 1000){
+            answer = n * 12000 + (k-n/10) * 2000;
+        }
+        return answer;
+    }
+}
